b5c8619b53ce31f0177db9830a4cb8e17dd2961f
Ralph Samuel <theralphsamuel@gmail.com>
2018-05-04 15:06:05 -0700

    Add test files, and clean up deployment stuff.

--- deployment/build-docker.sh
+++ deployment/build-docker.sh
@@ -11,9 +11,14 @@ mkdir -p $DIR/output
 echo "running in $DIR"
 
 #get go dep to manage dependencies
-echo "Downloading Godep..."
-#curl -fsSLz --progress-bar https://github.com/golang/dep/releases/download/v0.3.2/dep-linux-amd64 -o $DIR/output/dep
-chmod +x $DIR/output/dep
+if [ ! -e $DIR/output/dep ]
+then
+    echo "Downloading Godep..."
+    curl -fsSLz --progress-bar https://github.com/golang/dep/releases/download/v0.3.2/dep-linux-amd64 -o $DIR/output/dep
+    chmod +x $DIR/output/dep
+else
+    echo "Godep found, skipping download."
+fi
 
 #compile in a docker image
 echo "Compiling Go Binary..."
@@ -22,5 +27,5 @@ docker run --rm -v "$APP_SOURCE_DIR:/go/src" -v "$DIR/output:/usr/local/bin" -w 
 
 #put it all in our empty docker
 echo "Building html-to-pdf Docker"
-cd ./deployment && docker build -t html-to-pdf:latest .
+docker build -t html-to-pdf:latest .
 
---
+++ test/test-post.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -d "@test-html-body.html" -X POST http://localhost:9190/convert
